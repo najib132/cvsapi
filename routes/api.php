7334300a88fddad037e6dce3010edd3f81450f5d
Najib Marzouk <marzouknajib1988@gmail.com>
2022-11-13 15:02:24 -0500

    cvs project api rest

---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+
+
+
+
+
+// Route::get('cvs','CvController@index');
+// Route::post('cvs','CvController@store');
+Route::resource('cvs','CvController');
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
